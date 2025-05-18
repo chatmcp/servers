70b7cc49527021d74d51383857083807ea6aa681
idoubi <me@idoubi.cc>
2025-05-18 16:19:18 +0800

    update server fetch transport

---
+++ src/fetch/src/mcp_server_fetch/rest.py
@@ -0,0 +1,49 @@
+import contextlib
+from collections.abc import AsyncIterator
+
+from mcp.server.streamable_http_manager import StreamableHTTPSessionManager
+from starlette.applications import Starlette
+from starlette.routing import Mount
+from starlette.types import Receive, Scope, Send
+
+from mcp.server import Server
+
+async def start_http_server(server: Server, port: int, endpoint: str):
+    session_manager = StreamableHTTPSessionManager(
+        app=server,
+        event_store=None,
+        json_response=True,
+        stateless=True,
+    )
+
+    async def handle_streamable_http(
+        scope: Scope, receive: Receive, send: Send
+    ) -> None:
+        await session_manager.handle_request(scope, receive, send)
+
+    @contextlib.asynccontextmanager
+    async def lifespan(app: Starlette) -> AsyncIterator[None]:
+        """Context manager for session manager."""
+        async with session_manager.run():
+            try:
+                yield
+            finally:
+                pass
+
+    # Create an ASGI application using the transport
+    starlette_app = Starlette(
+        debug=False,
+        routes=[
+            Mount(endpoint, app=handle_streamable_http),
+        ],
+        lifespan=lifespan,
+    )
+
+    import uvicorn
+
+    host = "0.0.0.0"
+
+    config = uvicorn.Config(starlette_app, host=host, port=port, loop="asyncio")
+    server = uvicorn.Server(config)
+
+    await server.serve()
